60388c7d4b05e72980b98a166b3375d4966d6fbe
chris <867746469@qq.com>
2020-07-03 16:13:18 +0800

    添加webpack配置

---
+++ src/ticket/store/index.js
@@ -0,0 +1,9 @@
+import {applyMiddleware, combineReducers, createStore} from 'redux';
+import reducers from './reducers';
+import thunk from 'redux-thunk';
+
+export default createStore(
+	combineReducers(reducers),
+	{},
+	applyMiddleware(thunk)
+)
